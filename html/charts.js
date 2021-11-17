29cc6bf04af2ab613827d5ea2c77b7f9998e3362
maicent1 <676888309@qq.com>
2021-11-17 22:14:31 +0800

    新增寻访统计模块，以及更新了引导页

--- html/charts.js
+++ html/charts.js
@@ -1,4 +1,5 @@
 var laytpl = layui.laytpl;
+
 function fn(res) {
 	var main = echarts.init(document.getElementById('main'));
 	// console.log(res.data)
@@ -33,7 +34,7 @@ function fn(res) {
 			three.push(agent[i]);
 		}
 	}
-	
+
 	// 整合成echart饼图所需要的数据
 	var ChartData = [];
 	var sixData = {};
@@ -79,24 +80,73 @@ function fn(res) {
 		}]
 	};
 	main.setOption(option);
-	
-	
+
+	//计算六星抽取次数
 	times = 0;
 	RecentTimesArr = [];
-	for(var i = 0, l = six.length; i < l; i++){
+	for (var i = 0, l = six.length; i < l; i++) {
 		RecentTimes = six[i].times - times
 		times = six[i].times;
 		RecentTimesArr[i] = RecentTimes;
-		if (i == six.length-1){
-			RecentTimesArr[i+1] = agent.length - six[i].times;
+		if (i == six.length - 1) {
+			RecentTimesArr[i + 1] = agent.length - six[i].times;
 		}
 	}
-	for(var i = 0, l = six.length; i < l; i++){
-		six[i]['retimes'] = RecentTimesArr[i+1];
+	for (var i = 0, l = six.length; i < l; i++) {
+		six[i]['retimes'] = RecentTimesArr[i + 1];
 	}
+
+	//模板渲染《最近六星》
 	var getTpl = RecentSix.innerHTML,
 		view = document.getElementById('RecentSixView');
 	laytpl(getTpl).render(six, function(html) {
 		view.innerHTML = html;
+	});
+
+	//模板渲染《寻访统计》
+	var GachaSta = [{
+		"avg": 0,
+		"luck": "非酋",
+		"NoSix": 0,
+		"SixNum": 0,
+		"FiveNum": 0,
+		"FourNum": 0
+	}];
+	//计算寻访六星平均数
+	var SixTotal = 0;
+	for (var i = 0, l = six.length; i < l; i++) {
+		SixTotal += six[i].retimes
+	}
+	var SixAvg = parseInt(SixTotal / six.length);
+	GachaSta[0]['avg'] = SixAvg;
+	//计算非酋程度
+	if (SixAvg <= 30) {
+		GachaSta[0]['luck'] = "欧皇";
+	} else if (SixAvg > 30 && SixAvg <= 40) {
+		GachaSta[0]['luck'] = "正常人";
+	} else if (SixAvg > 40 && SixAvg <= 60) {
+		GachaSta[0]['luck'] = "非酋";
+	} else {
+		GachaSta[0]['luck'] = "超级大非酋";
+	}
+	//计算多久未出六星
+	GachaSta[0]['NoSix'] = six[0]['times'];
+	//计算各星级干员数量
+	GachaSta[0]['SixNum'] = six.length;
+	GachaSta[0]['FourNum'] = four.length;
+	GachaSta[0]['FiveNum'] = five.length;
+	console.log(GachaSta);
+	var sta = [{
+		"avg": 50,
+		"luck": "非酋",
+		"NoSix": 27,
+		"SixNum": 1,
+		"FiveNum": 20,
+		"FourNum": 20
+	}];
+	var getTpl2 = GachaStatistics.innerHTML,
+		view2 = document.getElementById('GachaStatisticsView');
+	laytpl(getTpl2).render(GachaSta, function(html) {
+		view2.innerHTML = html;
 	});
 }
--- index.py
+++ index.py
@@ -9,7 +9,7 @@ class index_mainWindow(QMainWindow):
     def __init__(self):
         super(index_mainWindow, self).__init__()
         self.setWindowTitle('明日方舟寻访统计工具')  # 窗口标题
-        self.setGeometry(5, 30, 1375, 750)  # 窗口的大小和位置设置
+        self.setGeometry(250, 150, 1375, 750)  # 窗口的大小和位置设置
         self.browser = QWebEngineView()
         # 加载html代码(这里注意html代码是用三个单引号包围起来的)
         self.browser.load(QUrl(QFileInfo("./html/index.html").absoluteFilePath()))
